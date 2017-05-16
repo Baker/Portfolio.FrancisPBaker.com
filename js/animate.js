15adb6bad81256c6eba52c2b9979482d8edf6d74
Francis <FrancisPBaker@gmail.com>
2017-05-16 19:05:59 -0400

    Added Animation + Delay

--- js/animate.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-   
-    $('div.card').on('click', function() {
-        $('div.card').addClass('animated rubberband').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-            $(this).removeClass('animated rubberband');
-        });
-    });
-});
